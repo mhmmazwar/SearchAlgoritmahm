91b2e1d4abfce1fbe9b9a1445306edbc3833c2fd
aswar <mhmmdazwar@gmail.com>
2022-10-24 16:25:49 +0700

    variabel arr

---
+++ SearchAlgoritmahm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgoritmahm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+       
+        static void Main(string[] args)
+        {
+        }
+    }
+}
